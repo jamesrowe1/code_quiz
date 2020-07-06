d8831464f2eaaf6d2e3f5243839441e2a61acc38
James Rowe <jamesrowe@outlook.com>
2020-07-05 20:27:30 -0400

    realized questions are objects

--- script.js
+++ script.js
@@ -2,3 +2,22 @@
   //timer
   //score
   //somehow high schore
+  //array of objects where the objects are questions
+
+
+//button to start quiz and timer
+  //watching all the time
+    //timer hit 0, game over
+  //loop ask question, give multiple choices
+    //right answer, increase score by one
+    //wrong answer, decrease timer by 5? seconds
+    //if no questions left
+      //end game
+  //loop
+
+//after end game
+  //give user score
+  //ask user for initials
+  //tell user what place they are in
+  //show leaderboard
+
