22d2b9ed86b1e4724d7c977b6e87d29ce264fb60
James Rowe <jamesrowe@outlook.com>
2020-07-08 16:27:07 -0400

    added high scores

--- script.js
+++ script.js
@@ -4,14 +4,15 @@
 //somehow high score
 //array of objects where the objects are questions, the possible solutions are an element, and the answer is another element
 
-
 //DOM vars
 var difficultyButtonsEl = document.getElementById("difficultyButtons");
 var answersEl = document.getElementById("answers");
 var questionsEl = document.getElementById("questions");
 var timerSpotEl = document.getElementById("timerSpot");
-var gameOverScreenEl = document.querySelector("#gameOverScreen")
-var gameOverMessageEl = document.querySelector(".modal-body")
+var gameOverScreenEl = document.querySelector("#gameOverScreen");
+var gameOverMessageEl = document.querySelector("#congrats");
+var initials = document.querySelector("#initials");
+var highScoreModalBody = document.querySelector("#highScoreModalBody");
 
 //Initialize Variables
 var timer;
@@ -20,6 +21,7 @@ var answerChose = true;
 var userDecision;
 var gameOver;
 var lengthOfQuiz = 0;
+var highScoresArray = [];
 
 //array of objects where the objects are questions, the possible solutions are an array, and the answer is another element
 var questionArray = [
@@ -29,31 +31,42 @@ var questionArray = [
   {
     q: "cd [answer here] changes you to the home directory",
     possibleAnswers: ["..", "!", "~", "It's not possible"],
-    correct: 2
+    correct: 2,
   },
   //2. What is the tag called attached to an image incase there is no image?
   //a: alt tag b: just-in-case tag c: picture tag d: error tag
   //correct: a
   {
     q: "What is the tag called attached to an image incase there is no image?",
-    possibleAnswers: ["alt tag", "just-in-case tag", "picture tag", "error tag"],
-    correct: 0
+    possibleAnswers: [
+      "alt tag",
+      "just-in-case tag",
+      "picture tag",
+      "error tag",
+    ],
+    correct: 0,
   },
   //3. What is the order, from first priority to last, in which CSS determines how to format things?
   //a: type, class, ID, order b: class, type, order, ID c: order, type, ID, class d: ID, class, type, order
   //correct: d
   {
-    q: "What is the order, from first priority to last, in which CSS determines how to format things?",
-    possibleAnswers: ["type, class, ID, order", "class, type, order, ID", "order, type, ID, class", "ID, class, type, order"],
-    correct: 3
+    q:
+      "What is the order, from first priority to last, in which CSS determines how to format things?",
+    possibleAnswers: [
+      "type, class, ID, order",
+      "class, type, order, ID",
+      "order, type, ID, class",
+      "ID, class, type, order",
+    ],
+    correct: 3,
   },
-  //4. Bootstrap works in grids of: 
+  //4. Bootstrap works in grids of:
   //a: 4 b: 8 c: 12 d: 16
   //correct: c
   {
     q: "Bootstrap works in grids of:",
     possibleAnswers: ["4", "8", "12", "16"],
-    correct: 2
+    correct: 2,
   },
   //5. What code do we use to request a true/false answer?
   //a: alert b: confirm c: prompt d: boolean
@@ -61,7 +74,7 @@ var questionArray = [
   {
     q: "What code do we use to request a true/false answer?",
     possibleAnswers: ["alert", "confirm", "prompt", "boolean"],
-    correct: 3
+    correct: 3,
   },
   //6. What can make up an array?
   //a: numbers b: strings c: booleans d: all of the above
@@ -69,38 +82,48 @@ var questionArray = [
   {
     q: "What can make up an array?",
     possibleAnswers: ["numbers", "strings", "booleans", "all of the above"],
-    correct: 0
+    correct: 0,
   },
   //7. How do we return a random integer between 1 and x?
-  //a: Math.random() b: Math.floor(Math.random*10+1) c: Math.random()*x d: Math.floor(Math.random*10) 
+  //a: Math.random() b: Math.floor(Math.random*10+1) c: Math.random()*x d: Math.floor(Math.random*10)
   //correct: d
   {
     q: "How do we return a random integer between 1 and x?",
-    possibleAnswers: ["Math.random()", "Math.floor(Math.random*10+1", "Math.random()*x", "Math.floor(Math.random*10)"],
-    correct: 1
+    possibleAnswers: [
+      "Math.random()",
+      "Math.floor(Math.random*10+1",
+      "Math.random()*x",
+      "Math.floor(Math.random*10)",
+    ],
+    correct: 1,
   },
   //8. JavaScript is:
   //a: Function Oriented b: Object Oriented c: Solving Oriented d: Parameter Oriented
   //correct: b
   {
     q: "JavaScript is:",
-    possibleAnswers: ["Function Oriented", "Object Oriented", "Solving Oriented", "Parameter Oriented"],
-    correct: 1
+    possibleAnswers: [
+      "Function Oriented",
+      "Object Oriented",
+      "Solving Oriented",
+      "Parameter Oriented",
+    ],
+    correct: 1,
   },
   //prepend question
   {
     q: "What command puts an element at the top of the page?",
     possibleAnswers: ["append", "prepend", "createElement", "querySelector"],
-    correct: 0
+    correct: 0,
   },
-]
+];
 
 function easyClick() {
-  lengthOfQuiz = Math.floor(questionArray.length * .34);
+  lengthOfQuiz = Math.floor(questionArray.length * 0.34);
   startQuiz();
 }
 function mediumClick() {
-  lengthOfQuiz = Math.floor(questionArray.length * .67);
+  lengthOfQuiz = Math.floor(questionArray.length * 0.67);
   startQuiz();
 }
 function hardClick() {
@@ -108,34 +131,42 @@ function hardClick() {
   startQuiz();
 }
 
+//updates the timer
 function updateTimer() {
   timerSpotEl.textContent = "Timer: " + timer;
 }
+
 //button to start quiz and timer
 function startQuiz() {
-  timer = 100;
+  //How long the game will last
+  timer = 75;
+  //Ensure gameOver= false so the game happens
   gameOver = false;
-  difficultyButtonsEl.setAttribute("style", "display:none")
-  questionsEl.setAttribute("style", "display: inline")
-  answersEl.setAttribute("style", "display: inline")
+
+  //hide the difficulty buttons
+  difficultyButtonsEl.setAttribute("style", "display:none");
+  //show the questions
+  questionsEl.setAttribute("style", "display: inline");
+  //show the possible answers
+  answersEl.setAttribute("style", "display: inline");
+  //update the timer spot
   updateTimer();
-  showQuestions()
+  showQuestions();
   var timeLeft = setInterval(function () {
     if (gameOver === true) {
       clearInterval(timeLeft);
     } else {
       timer--;
-    };
+    }
 
     updateTimer();
     //timer hit 0, game over
     if (timer <= 0) {
-      clearInterval(timeLeft)
+      clearInterval(timeLeft);
       gameOver = true;
       victory();
     } else if (gameOver === false) {
       //loop ask question, give multiple choices
-
     }
   }, 1000);
 }
@@ -143,15 +174,19 @@ function startQuiz() {
 //show the next question
 function showQuestions() {
   if (answerChose === true) {
-    document.getElementById("questions").textContent = questionArray[currentQuestion].q;
+    document.getElementById("questions").textContent =
+      questionArray[currentQuestion].q;
 
-    document.getElementById("answer0").textContent = questionArray[currentQuestion].possibleAnswers[0];
-    document.getElementById("answer1").textContent = questionArray[currentQuestion].possibleAnswers[1];
-    document.getElementById("answer2").textContent = questionArray[currentQuestion].possibleAnswers[2];
-    document.getElementById("answer3").textContent = questionArray[currentQuestion].possibleAnswers[3];
+    document.getElementById("answer0").textContent =
+      questionArray[currentQuestion].possibleAnswers[0];
+    document.getElementById("answer1").textContent =
+      questionArray[currentQuestion].possibleAnswers[1];
+    document.getElementById("answer2").textContent =
+      questionArray[currentQuestion].possibleAnswers[2];
+    document.getElementById("answer3").textContent =
+      questionArray[currentQuestion].possibleAnswers[3];
 
     answerChose = false;
-
   }
 }
 
@@ -180,14 +215,13 @@ function checkAnswer() {
     gameOver = true;
     victory();
   }
-
 }
 
 function victory() {
   //disappear questions and answers reappear difficulty choices
-  difficultyButtonsEl.setAttribute("style", "display:inline")
-  questionsEl.setAttribute("style", "display: none")
-  answersEl.setAttribute("style", "display: none")
+  difficultyButtonsEl.setAttribute("style", "display:inline");
+  questionsEl.setAttribute("style", "display: none");
+  answersEl.setAttribute("style", "display: none");
 
   //reset variables
   gameOver = true;
@@ -195,11 +229,59 @@ function victory() {
   answerChose = true;
 
   //set the Modal to pop up
-  gameOverMessageEl.textContent = "Congratulations! Your Score is: " + timer
+  gameOverMessageEl.textContent =
+    "Congratulations! Your Score is: " +
+    timer +
+    ". Please enter your initials below.";
   $("#gameOverScreen").modal("toggle");
-
 }
 
+//click submit button on game over screen
+function submitScore() {
+  var userInitScore = {
+    initials: initials.value,
+    score: timer,
+  };
+  //add the user initials and score to the high score array
+  highScoresArray.push(userInitScore);
+
+  //sorting high score array by score
+  var sortedHighScoreArray = highScoresArray.sort(function (a, b) {
+    return b.score - a.score;
+  });
+  localStorage.setItem("highscores", JSON.stringify(sortedHighScoreArray));
+  closeModal();
+}
+
+//view the high scores in a modal
+function viewHighScores() {
+  //get the sorted High Score array from local storage
+  var sortedHighScoreArray = JSON.parse(localStorage.getItem("highscores"));
+  //clear the modal
+  highScoreModalBody.textContent = "";
+  for (var i = 0; i < sortedHighScoreArray.length; i++) {
+    var score = document.createElement("P");
+    score.innerText =
+      Number(i + 1) +
+      ". " +
+      sortedHighScoreArray[i].initials +
+      "---" +
+      sortedHighScoreArray[i].score;
+    console.log(score.textContent);
+    highScoreModalBody.append(score.textContent);
+
+    //put the next score one line below
+    var linebreak = document.createElement("br");
+    highScoreModalBody.append(linebreak);
+  }
+}
+
+//clear the high scores
+function clearHighScores() {
+  highScoreModalBody.textContent = "";
+  localStorage.removeItem("highscores");
+  highScoresArray = [];
+}
 function closeModal() {
   //close the modal
   $("#gameOverScreen").modal("toggle");
