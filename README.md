3a0eb60d360015ae937f13a6f87facc348b2e41c
James Rowe <jamesrowe@outlook.com>
2020-07-08 10:05:05 -0400

    added readme and difficulties

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # code_quiz
+
+## Project Title
+
+This is the 4th Homework Assignment for the Columbia Coding Bootcamp. It is a Code Quiz. Prepare to be challenged!
+
+## Getting Started
+
+Begin by selecting a difficulty. The higher the difficulty, the more questions you must answer in the same amount of time.
+## Prerequisites
+
+You must have a working web browser. Knowledge of coding and coding terms is suggested.
+
+## Built With
+
+- Visual Studio Code
+- HTML
+- CSS
+- JavaScript
+- Chrome
+- Hosted by Github
+
+## Authors
+
+James Rowe
+
+## License
+
+Licensed under Awesome Coding L.L.C. (not a real company)
+
+## Troubleshooting
+
+For any issues contact James Rowe
+
+## Acknowledgments
+
+- Ben Wright
+- Justin Simero
+- Wilson Linares
+- And my study group:
+- Justin Ramirez
+- Michael Haber
+- Gabriel Hernandez
+- Tiffany Lombardi
+- Melissa Mercado
--- script.js
+++ script.js
@@ -5,68 +5,128 @@
 //array of objects where the objects are questions, the possible solutions are an element, and the answer is another element
 
 
-var timer = 10;
-var currentQuestion = -1;
+//DOM vars
+var difficultyButtonsEl = document.getElementById("difficultyButtons");
+var answersEl = document.getElementById("answers");
+var questionsEl = document.getElementById("questions");
+var timerSpotEl = document.getElementById("timerSpot");
+var gameOverScreenEl = document.querySelector("#gameOverScreen")
+var gameOverMessageEl = document.querySelector(".modal-body")
+
+//Initialize Variables
+var timer;
+var currentQuestion = 0;
 var answerChose = true;
 var userDecision;
-var gameOver = false;
+var gameOver;
+var lengthOfQuiz = 0;
+
+//array of objects where the objects are questions, the possible solutions are an array, and the answer is another element
 var questionArray = [
+  //1. cd [answer here] changes you to the home directoy
+  //a: .. b: ! c: ~ d: That's not possible
+  //correct: c
   {
     q: "cd [answer here] changes you to the home directory",
     possibleAnswers: ["..", "!", "~", "It's not possible"],
     correct: 2
   },
+  //2. What is the tag called attached to an image incase there is no image?
+  //a: alt tag b: just-in-case tag c: picture tag d: error tag
+  //correct: a
   {
     q: "What is the tag called attached to an image incase there is no image?",
     possibleAnswers: ["alt tag", "just-in-case tag", "picture tag", "error tag"],
     correct: 0
   },
+  //3. What is the order, from first priority to last, in which CSS determines how to format things?
+  //a: type, class, ID, order b: class, type, order, ID c: order, type, ID, class d: ID, class, type, order
+  //correct: d
+  {
+    q: "What is the order, from first priority to last, in which CSS determines how to format things?",
+    possibleAnswers: ["type, class, ID, order", "class, type, order, ID", "order, type, ID, class", "ID, class, type, order"],
+    correct: 3
+  },
+  //4. Bootstrap works in grids of: 
+  //a: 4 b: 8 c: 12 d: 16
+  //correct: c
+  {
+    q: "Bootstrap works in grids of:",
+    possibleAnswers: ["4", "8", "12", "16"],
+    correct: 2
+  },
+  //5. What code do we use to request a true/false answer?
+  //a: alert b: confirm c: prompt d: boolean
+  //correct: d
+  {
+    q: "What code do we use to request a true/false answer?",
+    possibleAnswers: ["alert", "confirm", "prompt", "boolean"],
+    correct: 3
+  },
+  //6. What can make up an array?
+  //a: numbers b: strings c: booleans d: all of the above
+  //correct: d
+  {
+    q: "What can make up an array?",
+    possibleAnswers: ["numbers", "strings", "booleans", "all of the above"],
+    correct: 0
+  },
+  //7. How do we return a random integer between 1 and x?
+  //a: Math.random() b: Math.floor(Math.random*10+1) c: Math.random()*x d: Math.floor(Math.random*10) 
+  //correct: d
+  {
+    q: "How do we return a random integer between 1 and x?",
+    possibleAnswers: ["Math.random()", "Math.floor(Math.random*10+1", "Math.random()*x", "Math.floor(Math.random*10)"],
+    correct: 1
+  },
+  //8. JavaScript is:
+  //a: Function Oriented b: Object Oriented c: Solving Oriented d: Parameter Oriented
+  //correct: b
+  {
+    q: "JavaScript is:",
+    possibleAnswers: ["Function Oriented", "Object Oriented", "Solving Oriented", "Parameter Oriented"],
+    correct: 1
+  },
+  //prepend question
+  {
+    q: "What command puts an element at the top of the page?",
+    possibleAnswers: ["append", "prepend", "createElement", "querySelector"],
+    correct: 0
+  },
 ]
 
-
-//here are questions. Maybe think about randomizing answer order?
-//1. cd [answer here] changes you to the home directoy
-//a: .. b: ! c: ~ d: That's not possible
-//correct: c
-//2. What is the tag called attached to an image incase there is no image?
-//a: alt tag b: just-in-case tag c: picture tag d: error tag
-//correct: a
-//3. What is the order, from first priority to last, in which CSS determines how to format things?
-//a: type, class, ID, order b: class, type, order, ID c: order, type, ID, class d: ID, class, type, order
-//correct: d
-//4. Bootstrap works in grids of: 
-//a: 4 b: 8 c: 12 d: 16
-//correct: c
-//5. What code do we use to request a true/false answer?
-//a: alert b: confirm c: prompt d: boolean
-//correct: d
-//6. What can make up an array?
-//a: numbers b: strings c: booleans d: all of the above
-//correct: d
-//7. How do we return a random integer between 1 and x?
-//a: Math.random() b: Math.random()*x c: Math.floor(Math.random*10) d: Math.floor(Math.random*10+1)
-//correct: d
-//8. JavaScript is:
-//a: Function Oriented b: Object Oriented c: Solving Oriented d: a programming language
-//correct: both b and d (trick question?)
-
+function easyClick() {
+  lengthOfQuiz = Math.floor(questionArray.length * .34);
+  startQuiz();
+}
+function mediumClick() {
+  lengthOfQuiz = Math.floor(questionArray.length * .67);
+  startQuiz();
+}
+function hardClick() {
+  lengthOfQuiz = questionArray.length;
+  startQuiz();
+}
 
 //button to start quiz and timer
-function easyClick() {
-  document.getElementById("difficultyButtons").setAttribute("style", "display:none")
-  document.getElementById("answers").setAttribute("style", "display: inline")
-  document.getElementById("timerSpot").textContent = "Timer: " + timer;
+function startQuiz() {
+  timer = 100;
+  gameOver = false;
+  difficultyButtonsEl.setAttribute("style", "display:none")
+  questionsEl.setAttribute("style", "display: inline")
+  answersEl.setAttribute("style", "display: inline")
+  timerSpotEl.textContent = "Timer: " + timer;
   showQuestions()
   var timeLeft = setInterval(function () {
     if (gameOver === true) {
       clearInterval(timeLeft);
-      victory();
+    } else {
+      timer--;
     };
 
-    timer--;
-    document.getElementById("timerSpot").textContent = "Timer: " + timer;
+    timerSpotEl.textContent = "Timer: " + timer;
     //timer hit 0, game over
-    if (timer === 0) {
+    if (timer <= 0) {
       clearInterval(timeLeft)
       gameOver = true;
       victory();
@@ -80,8 +140,8 @@ function easyClick() {
 //show the next question
 function showQuestions() {
   if (answerChose === true) {
-    currentQuestion++;
-    document.getElementById("question").textContent = questionArray[currentQuestion].q;
+    console.log("show questions" + currentQuestion)
+    document.getElementById("questions").textContent = questionArray[currentQuestion].q;
 
     document.getElementById("answer0").textContent = questionArray[currentQuestion].possibleAnswers[0];
     document.getElementById("answer1").textContent = questionArray[currentQuestion].possibleAnswers[1];
@@ -105,20 +165,40 @@ function userChose(choice) {
 
 //check the answer and subtract 5 seconds if wrong
 function checkAnswer() {
-
+  //if user gets wrong, timer drops 5 seconds
   if (userDecision !== questionArray[currentQuestion].correct) {
     timer = timer - 5;
   }
+  //increment currentQuestion
+  currentQuestion++;
 
-  console.log(currentQuestion)
-  console.log(questionArray.length)
-  if (currentQuestion === questionArray.length - 1) {
+  //if currentQuestion is the last question, game is over
+  if (currentQuestion === lengthOfQuiz) {
     gameOver = true;
+    victory();
   }
 
 }
 
 function victory() {
-  alert("Your score is " + timer)
+  //disappear questions and answers reappear difficulty choices
+  difficultyButtonsEl.setAttribute("style", "display:inline")
+  questionsEl.setAttribute("style", "display: none")
+  answersEl.setAttribute("style", "display: none")
+
+  //reset variables
+  gameOver = true;
+  currentQuestion = 0;
+  console.log("victory" + currentQuestion)
+  answerChose = true;
+
+  //set the Modal to pop up
+  gameOverMessageEl.textContent = "Congratulations! Your Score is: " + timer
+  $("#gameOverScreen").modal("toggle");
+
 }
 
+function closeModal() {
+  //close the modal
+  $("#gameOverScreen").modal("toggle");
+}
